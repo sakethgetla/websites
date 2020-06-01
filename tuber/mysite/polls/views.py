81fea78e739dfa6b82390b5253b5742667ac4c8f
sakethgetla <saketh.getla@gmail.com>
2020-06-01 13:47:36 +1000

    autopushed

--- tuber/mysite/polls/views.py
+++ tuber/mysite/polls/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hekko")
+
+def details(request, question_id):
+    return HttpResponse("you r looking at the question %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("you r looking at the results of question %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you r voting on question %s" % question_id)
