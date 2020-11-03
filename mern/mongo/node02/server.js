433bf556c13ac71a0471cd35935e83dbb31efc78
sakethgetla <saketh.getla@protonmail.com>
2020-11-03 15:35:37 +1100

    cant insert with using mongdb. trying mongoose

--- mern/mongo/node02/server.js
+++ mern/mongo/node02/server.js
@@ -2,26 +2,20 @@ const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
 
-const url = 'mongodb://localhost:27017'
+const url = 'mongodb://localhost:27017';
 
-app.get('/api/users',(req,res)=>{
+const cars = [
+    {model: "a", year: 12},
+    {model: "b", year: 32},
+    {model: "c", year: 38}
+]
+app.get('/api/users', (req,res)=>{
     MongoClient.connect(url, {useUnifiedTopology: true}, (err, client)=>{
-        if(err){
-            console.log('couldnt connect')
-        } else {
-            console.log('connected')
-        }
-        client.db('Cars').collection('items').insertOne({
-            model:"ford",
-            year:2020
-        }), (err, res)=>{
+        client.db('Car').collection('items').insertMany(cars, async (err, res)=>{
             if(err){
-                console.log('not inserted')
-            } else {
-                console.log('inserted')
+                return console.log(`Error: ${err}`)
             }
-
-        }
+        })
         client.close()
     })
 })
