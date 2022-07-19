f768666cc01d65507b204ad55fad696b15a4abe7
sakethgetla <saketh.getla@protonmail.com>
2022-07-19 15:53:41 +1000

    got image rec to work :)

--- AI/img_rec/src/App.tsx
+++ AI/img_rec/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import '@tensorflow/tfjs-backend-cpu';
+import * as tf from '@tensorflow/tfjs';
+// import {browser} as tf from '@tensorflow/tfjs';
+
 // import logo from './logo.svg';
 // import './App.css';
 // import { mobilenet } from '@tensorflow-models/mobilenet';
@@ -21,10 +24,28 @@ function App() {
 
   // useEffect(async () => {
   useEffect(() => {
+
+    const im = new Image();
+    im.src = "./images/soilders.jpg";
+    im.onload = () => {
+      // const a = tf.browser.fromPixels(im, 3);
+      const a = tf.browser.fromPixels(im);
+      a.print();
+      console.log(a.shape);
+      mobilenet.load().then((model) => {
+        console.log('here: ');
+        model.classify(a).then((out) => {
+          console.log(out);
+        });
+      });
+    }
+
     // const model = await mobilenet.load();
-    mobilenet.load().then(() => {
-      console.log('here: ');
-    });
+
+    // mobilenet.load().then(() => {
+    //   console.log('here: ');
+    // });
+
     // var out = await model.classify(img);
 
     // model.classify(img).then((out) => {
