f871000103390d33c3490b5b68c78e131af9ec7e
sakethgetla <saketh.getla@protonmail.com>
2022-09-06 14:37:47 +1000

    finally got website working with parcel :)

--- scratch/game/js/engine.js
+++ scratch/game/js/engine.js
@@ -1,6 +1,8 @@
 // require('@tensorflow/tfjs-backend-cpu');
 // var tf = require( '@tensorflow/tfjs-core');
 // import * as tf from;
+import '@tensorflow/tfjs-backend-cpu';
+import * as tf from '@tensorflow/tfjs-core';
 
 var Engine = function(global) {
   var doc = global.document,
@@ -15,16 +17,21 @@ var Engine = function(global) {
   const FPS = 50;
   const timestep = 1000 / FPS;
   var x = 0;
+  let frameSize = [500, 500];
+  const numBodies = 15;
   // let bodies: { pos: tf.Tensor, vel: tf.Tensor, size: number };
 
 
 
   function init() {
     console.log('init');
-    // let pos = tf.randomUniform([numBodies, 2], frameSize[0] * 0.3, frameSize[0] * 0.7);
-    // let vel = tf.randomUniform([numBodies, 2], -0.1, 0.2);
-    // let vel = tf.rand([numBodies, 2]);
-    let pos = null, vel = null;
+    console.log('init');
+    let pos = tf.randomUniform([numBodies, 2], 0, frameSize[0] * 0.2);
+    let vel = tf.randomUniform([numBodies, 2], -0.1, 0.2);
+    // let pos = null, vel = null;
+    // vel = tf.tensor([1,2,3])
+    // vel = tf.rand
+    vel.print();
     bodies = { pos, vel, size: 5 };
 
     main();
@@ -35,7 +42,7 @@ var Engine = function(global) {
     // console.log('main');
     var now = Date.now(),
       dt = now - lastTime;
-    console.log('time', now, lastTime, dt);
+    // console.log('time', now, lastTime, dt);
 
     if (dt > timestep) {
       update();
@@ -54,12 +61,86 @@ var Engine = function(global) {
 
   function update() {
     ctx.beginPath();
-    ctx.clearRect(0, 0, 500, 500);
-    ctx.strokeRect(0, 0, 500, 500);
-    ctx.rect(x, x, 100, 100);
-    ctx.fill();
-    // canvas.getContext('2d').begin()
-    x++;
+
+    ctx.clearRect(0, 0, frameSize[0], frameSize[1]);
+    ctx.strokeRect(0, 0, frameSize[0], frameSize[1]);
+
+
+    // cohesion
+    let avgPos = tf.sum(bodies.pos, 0);
+    avgPos = tf.sub(avgPos, bodies.pos);
+    avgPos = tf.mul(avgPos, 1 / (numBodies - 1))
+    avgPos = tf.sub(avgPos, bodies.pos);
+    // let forces = tf.mul(avgPos, 0.001);
+    // forces = tf.mul(avgPos, 0.001);
+    forces = tf.divNoNan(avgPos, 500);
+    // forces.print();
+    bodies.vel = tf.add(bodies.vel, forces);
+
+
+    // alignment
+    // add all velocities
+
+    let avgVel = tf.sum(bodies.vel, 0);
+    avgVel = tf.sub(avgVel, bodies.vel);
+    avgVel = tf.divNoNan(avgVel, (numBodies-1)*100);
+    // avgVel = tf.mul(avgVel, 0.001/(numBodies-1));
+    bodies.vel = tf.add(bodies.vel, avgVel);
+
+
+
+    // separation
+
+    let x = tf.zeros([numBodies, numBodies, 2]);
+
+    x = tf.add(x, bodies.pos)
+    // x.print();
+    // console.log(x.shape);
+
+    x = tf.sub(tf.transpose(x, [1, 0, 2]), x);
+
+    let y;
+    y = tf.pow(x, 2);
+    y = tf.sum(y, 2);
+    // y = tf.pow(y, -1.1);
+    y = tf.divNoNan(1, y);
+    // y.print();
+    // console.log(y.shape);
+
+    // x =
+    // y = tf.clipByValue(y, 0, 10);
+    // x = tf.clipByValue(x, 0, 10);
+    x = tf.transpose(tf.mul(tf.transpose(x), tf.transpose(y)));
+
+    // x.print();
+    // x = tf.clipByValue(x, 0, 10);
+    // x.print();
+
+    x = tf.sum(x, 1);
+    // x.print();
+
+    bodies.vel = tf.add(bodies.vel, x);
+
+
+
+
+    let pos = bodies.pos.bufferSync();
+    let dirPos = tf.add(bodies.pos, tf.mul(bodies.vel, 15)).bufferSync();
+
+    for (var i = 0; i < numBodies; ++i) {
+      ctx.beginPath();
+      ctx.arc(pos.get(i, 0), pos.get(i, 1), bodies.size, 0, 2 * Math.PI);
+      ctx.moveTo(pos.get(i, 0), pos.get(i, 1));
+      ctx.lineTo(dirPos.get(i, 0), dirPos.get(i, 1));
+      ctx.stroke();
+    }
+
+      bodies.pos = tf.add(bodies.pos, bodies.vel);
+
+    // ctx.rect(x, x, 100, 100);
+    // ctx.fill();
+    // // canvas.getContext('2d').begin()
+    // x++;
 
   }
 
